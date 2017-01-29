e8558e99e03977938c8a4d0fbf922c8568f8128b
Rex Vizconde <vizconder@ochin.org>
2017-01-29 14:24:16 -0800

    Added xUnit

---
+++ PlayingCards/TestPlayingCards_xUnit/PlayingCardsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PlayingCards;
+
+namespace TestPlayingCards_xUnit
+{
+    public class PlayingCardsTest
+    {
+        [Fact]
+        public void Card()
+        {
+            Suit suit = Suit.Spades;
+
+            Rank rnk = Rank.King;
+
+            Card crd = new Card(suit, rnk);
+
+            Assert.Equal((int)suit, crd.CardSuit);
+
+            Assert.Equal((int)rnk, crd.SuitRank);
+
+            Assert.Equal(0, crd.DeckRank);
+
+            Assert.Equal(String.Format("{0} of {1}", rnk, suit), crd.CardName);
+        }
+    }
+}
